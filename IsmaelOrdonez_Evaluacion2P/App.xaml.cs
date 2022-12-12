948eef610133f7c16de52e0178bebcddf4793e32
Ismael Ordoñez <118380297+JILogio@users.noreply.github.com>
2022-12-12 08:55:19 -0500

    Initial commit

---
+++ IsmaelOrdonez_Evaluacion2P/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace IsmaelOrdonez_Evaluacion2P;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ IsmaelOrdonez_Evaluacion2P/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace IsmaelOrdonez_Evaluacion2P;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void recargar(object sender,EventArgs e)
+    {
+		
+    }
+
+}
